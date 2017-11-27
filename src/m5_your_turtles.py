4c3dafe2f2488d16ec20002120aba07e8e56c721
sethupa <sethupa@rose-hulman.edu>
2017-11-27 15:13:11 -0500

    m3 done

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Adi Sethupathy.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,33 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+size = 100
+
+adi = rg.SimpleTurtle('turtle')
+adi.pen = rg.Pen('red', 5)
+adi.speed = 15
+
+mo = rg.SimpleTurtle()
+mo.pen = rg.Pen('blue', 40)
+mo.speed = adi.speed
+
+for k in range(20):
+    adi.draw_circle(size)
+
+    adi.pen_up()
+    adi.forward(10)
+    adi.pen_down()
+
+
+    mo.draw_square(size)
+    mo.pen_up()
+    mo.forward(10)
+    mo.pen_down()
+    size = size - 12
+
+
+window.close_on_mouse_click()
